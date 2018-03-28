3bd2c2652bd2552b603762d76633b7c73740913e
snarksliveshere <snarksliveshere@gmail.com>
2018-03-28 17:15:17 +0300

    28-03. Добавил похожие товары, поставил стандартный слайдер на главную в блок рекомендуемые

--- build/js/custom.js
+++ build/js/custom.js
@@ -1,37 +1,37 @@
 // jsDesc: slick set
-    $('.recommended_products .products_list').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        arrows: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 99999,
-                settings: "unslick"
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-
-        ]
-    });
+//     $('.recommended_products .products_list').slick({
+//         dots: true,
+//         infinite: false,
+//         speed: 300,
+//         arrows: false,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         responsive: [
+//             {
+//                 breakpoint: 99999,
+//                 settings: "unslick"
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 2,
+//                     slidesToScroll: 1
+//                 }
+//             },
+//             {
+//                 breakpoint: 600,
+//                 settings: {
+//                     slidesToShow: 2,
+//                     slidesToScroll: 1
+//                 }
+//             },
+//             {
+//                 breakpoint: 480,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//
+//         ]
+//     });
