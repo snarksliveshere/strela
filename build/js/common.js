6ac75328e499712c657b527aa9e081fe27270196
snarksliveshere <snarksliveshere@gmail.com>
2018-04-12 13:00:18 +0300

    12-04 Форма заказа. v. 0.9

--- build/js/common.js
+++ build/js/common.js
@@ -8,20 +8,17 @@
 //         });
 //     }
 // }
-
-$(function() {
-
+$(function () {
     function changeIcon(icon) {
-        if(icon.hasClass('fa-angle-down')) {
+        if (icon.hasClass('fa-angle-down')) {
             icon.removeClass('fa-angle-down').addClass('fa-angle-up');
         } else {
             icon.removeClass('fa-angle-up').addClass('fa-angle-down');
         }
     }
+
     var body = $('body');
-
     var mql = window.matchMedia('all and (max-width: 767px)');
-
     if (mql.matches) {
         // jsDesc: i see u menu
         var seeMenu = $('.iseeu_menu');
@@ -29,29 +26,27 @@ $(function() {
         $('.header__top .menu').clone().appendTo(seeMenu).removeClass('row');
         $('.header__menu .menu').clone().appendTo(seeMenu).removeClass('row');
         $('.header__mobile-separator .mobile_click').click(function () {
-           $('body').prepend($('<div class="veil"></div>'));
+            $('body').prepend($('<div class="veil"></div>'));
             seeMenu.toggle();
         });
-        $('body').on('click','.veil',function () {
-               seeMenu.toggle();
-               $(this).remove();
+        $('body').on('click', '.veil', function () {
+            seeMenu.toggle();
+            $(this).remove();
         });
-        if($('.icon-menu').length) {
+        if ($('.icon-menu').length) {
             $('.icon-menu').click(function () {
                 $(this).next().slideToggle();
                 var icon = $(this).find('i');
                 changeIcon(icon);
             });
         }
-
         // jsDesc: кнопка фильтры на мобильной версии
         var toggle_filter = $('.filters__form-button');
         toggle_filter.click(function () {
             var block = $(this).next().find('.jq-selectbox__dropdown');
             $(this).next().slideToggle();
-
             block.each(function () {
-                if($(this).css('display') == 'block') {
+                if ($(this).css('display') == 'block') {
                     $(this).toggle();
                 }
             });
@@ -59,7 +54,6 @@ $(function() {
             changeIcon(icon);
             $('.filters__form-sort').toggleClass('pt-5');
         });
-
         // jsDesc: переносим корзину
         $('.header__basket-wrap').prependTo($('.header__mobile-separator'));
     } // end of resize < 768
@@ -67,13 +61,13 @@ $(function() {
         // jsDesc: main menu animation
         $('.main_menu .dropmenu').hover(
             function () {
-                if($(this).parent().hasClass('submenu')) {
+                if ($(this).parent().hasClass('submenu')) {
                     $(this).find('a').eq(0).addClass('pale_bg');
                 }
                 $(this).find('ul:first').fadeIn();
             },
             function () {
-                if($(this).parent().hasClass('submenu')) {
+                if ($(this).parent().hasClass('submenu')) {
                     $(this).find('a').eq(0).removeClass('pale_bg');
                 }
                 $(this).find('ul:first').delay(200).stop().fadeOut();
@@ -84,11 +78,11 @@ $(function() {
             $('.header__search-form').toggle();
         });
         // jsDesc: icon menu для разрежений более 768
-        if($('.icon-menu').length) {
+        if ($('.icon-menu').length) {
             var iconMenu = $('.icon-menu');
             iconMenu.find('i').removeClass('fa-angle-down').addClass('fa-angle-right');
             iconMenu.click(function () {
-                if($(this).hasClass('imclicked')) {
+                if ($(this).hasClass('imclicked')) {
                     $(this).next().hide();
                     $(this).removeClass('imclicked');
                 } else {
@@ -98,7 +92,6 @@ $(function() {
             });
         }
     } // end of Resize > 768
-
     // jsDesc: slick set
     $('.aside_slider').slick({
         dots: true,
@@ -134,7 +127,6 @@ $(function() {
                     slidesToScroll: 1
                 }
             }
-
         ]
     });
     $('.quarter_slider').slick({
@@ -176,7 +168,6 @@ $(function() {
                     slidesToScroll: 1
                 }
             }
-
         ]
     });
     $('.quarter_slider-works').slick({
@@ -211,10 +202,8 @@ $(function() {
                     slidesToScroll: 1
                 }
             }
-
         ]
     });
-
     $('.good_slider-main').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -230,28 +219,24 @@ $(function() {
         arrows: false,
         focusOnSelect: true
     });
-
     // jsDesc: fancybox
     $(".fancybox").fancybox({
-       buttons: ['close']
+        buttons: ['close']
     });
-
-    $('[data-event]').on('click',function() {
+    $('[data-event]').on('click', function () {
         if ($(this).data('event') == 'fancy') {
             $.fancybox.open({
-                src  : '#'+$(this).data('name'),
+                src: '#' + $(this).data('name'),
             });
         }
     });
-
     // jsDesc: tabs good
-    $('.good_tabs .tabset li').click(function() {
+    $('.good_tabs .tabset li').click(function () {
         var ind = $(this).index();
         $(this).addClass('active gradient').siblings().removeClass('active gradient');
         // $('.good_tabs .tab').eq(ind).addClass('active').siblings().removeClass('active');
         $(this).parents('.good_tabs').find('.tab').eq(ind).addClass('active').siblings().removeClass('active');
     });
-
     // jsDesc: - & + buttons in good
     $('.minus').click(function () {
         var $input = $(this).next();
@@ -265,26 +250,25 @@ $(function() {
         $input.val(parseInt($input.val()) + 1);
         $input.change();
     });
-
     //form styling
     $('.filters__form select').styler({
-        selectSmartPositioning:false
+        selectSmartPositioning: false
     });
-    if($('.filters__form').length){
+    if ($('.filters__form').length) {
         var toggle_label = $('.filters__form-select-label');
-        toggle_label.click(function(){
+        toggle_label.click(function () {
             var icon = $(this).find('i');
             var block = $(this).parent().siblings().find('.jq-selectbox__dropdown');
             $(this).parent().find('.jq-selectbox__dropdown').slideToggle();
             block.each(function () {
-               if($(this).css('display') == 'block') {
-                   $(this).slideToggle();
-               }
+                if ($(this).css('display') == 'block') {
+                    $(this).slideToggle();
+                }
             });
             changeIcon(icon);
         });
     }
-        // adding filter tags
+    // adding filter tags
     var filterLi = $('.filters__form-select .jq-selectbox__dropdown li');
     var filterName = [];
     filterLi.click(function () {
@@ -292,8 +276,8 @@ $(function() {
         var icon = $(this).parents('.filters__form-modification').find('i');
         var name = $(this).parents('.filters__form-modification').find('.filters__form-select-label').text();
         changeIcon(icon);
-        var htmlBlock = $('<div class="d-inline-block mr-2 mb-2"><span class="pale_bg py-1 px-2 d-inline-block">' + name + '(<span class="fwb">'+ text +'</span>)</span><a href="#" class="ml-1 pale_bg filters__form-available-button"><i class="fas fa-times-circle"></i></a></div>');
-        if($.inArray(text, filterName) == -1) {
+        var htmlBlock = $('<div class="d-inline-block mr-2 mb-2"><span class="pale_bg py-1 px-2 d-inline-block">' + name + '(<span class="fwb">' + text + '</span>)</span><a href="#" class="ml-1 pale_bg filters__form-available-button"><i class="fas fa-times-circle"></i></a></div>');
+        if ($.inArray(text, filterName) == -1) {
             filterName.push(text);
             htmlBlock.appendTo('.filters__form-available');
         }
@@ -303,21 +287,18 @@ $(function() {
         $('.filters__form-available .d-inline-block').remove();
         filterName = [];
     });
-    body.on('click','.filters__form-available-button', function () {
+    body.on('click', '.filters__form-available-button', function () {
         var text = $(this).parent().find('.fwb').text();
-        filterName.splice($.inArray(text, filterName),1);
+        filterName.splice($.inArray(text, filterName), 1);
         $(this).parent().remove();
         return false;
     });
-
     // stuff
     $('.return_false').click(function () {
-       return false;
+        return false;
     });
-
     // jsDesc: basket
-    $('body').on('click','.shopping',function(){
+    $('body').on('click', '.shopping', function () {
         $(this).next().toggle();
     });
-
 });
