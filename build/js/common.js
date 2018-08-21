5c34eb7255d20a0d99b353cbb091e5603aa391ca
snarksliveshere <snarksliveshere@gmail.com>
2018-08-21 16:50:41 +0300

    new header menu

--- build/js/common.js
+++ build/js/common.js
@@ -59,20 +59,20 @@ $(function () {
     } // end of resize < 768
     else {
         // jsDesc: main menu animation
-        $('.main_menu .dropmenu').hover(
-            function () {
-                if ($(this).parent().hasClass('submenu')) {
-                    $(this).find('a').eq(0).addClass('pale_bg');
-                }
-                $(this).find('ul:first').fadeIn();
-            },
-            function () {
-                if ($(this).parent().hasClass('submenu')) {
-                    $(this).find('a').eq(0).removeClass('pale_bg');
-                }
-                $(this).find('ul:first').delay(200).stop().fadeOut();
-            }
-        );
+        // $('.main_menu .dropmenu').hover(
+        //     function () {
+        //         if ($(this).parent().hasClass('submenu')) {
+        //             $(this).find('a').eq(0).addClass('pale_bg');
+        //         }
+        //         // $(this).find('ul:first').fadeIn();
+        //     },
+        //     function () {
+        //         if ($(this).parent().hasClass('submenu')) {
+        //             $(this).find('a').eq(0).removeClass('pale_bg');
+        //         }
+        //         // $(this).find('ul:first').delay(200).stop().fadeOut();
+        //     }
+        // );
         // jsDesc: search form
         $('.header__search-button').click(function () {
             $('.header__search-form').toggle();
